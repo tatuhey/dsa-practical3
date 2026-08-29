1fc1f634141ef711971202c530e9da40dd65ecb8
tatuhey <17900375@student.curtin.edu.au>
2026-08-29 15:41:29 +0800

    changed DSAq to abstract class, changed variables to protected from private, added shuffleQueue class and their overriding methods

--- DSAQueue.java
+++ DSAQueue.java
@@ -1,8 +1,9 @@
-public class DSAQueue {
+public abstract class DSAQueue {
     
-    private Object[] queue;
-    private int count;
-    private int DEFAULT_CAPACITY = 100;
+    // cannot be private
+    protected Object[] queue;
+    protected int count;
+    protected int DEFAULT_CAPACITY = 100;
 
     // default constructor
     public DSAQueue() {
@@ -29,8 +30,22 @@ public class DSAQueue {
         return count == queue.length;
     }
 
+    // they are defined in the subclasses (shuffleQ and circularQ)
+    // https://math.hws.edu/eck/cs124/javanotes6/c5/s5.html
+    // https://stackoverflow.com/questions/79411745/polymorphism-and-abstract-classes-in-java
+    // https://www.w3schools.com/java/java_polymorphism.asp
+    // PDI lecture06
+
+    public abstract void enqueue(Object value);
+    public abstract Object dequeue(Object value);
+    public abstract Object peek();
+}
+
+
+class shuffleQueue extends DSAQueue {
+
     // https://stackoverflow.com/questions/18141596/which-exception-to-throw-when-user-tries-to-add-to-a-full-container
-    // mutators
+    @Override
     public void enqueue(Object value) {
         if (isFull())
             throw new IllegalStateException("Queue is full");
@@ -40,6 +55,7 @@ public class DSAQueue {
         }
     }
 
+    @Override
     public Object dequeue(Object value) {
         Object frontVal = peek();
         for(int i = 1; i < count-1 ; i++) {
@@ -49,12 +65,11 @@ public class DSAQueue {
         return frontVal;
     }
 
+    @Override
     public Object peek() {
         if (isEmpty())
             throw new IllegalStateException("Queue is empty");
         else
             return queue[0];
-        
-        
     }
 }
