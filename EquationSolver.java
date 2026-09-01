c2e515f6fbf7c0510b71327f60d55e0cf2db817d
raihan-mint <tatuhey@gmail.com>
2026-09-01 12:48:18 +0800

    someprogress i guess

--- EquationSolver.java
+++ EquationSolver.java
@@ -24,30 +24,65 @@ public class EquationSolver {
     public static double solve(String equation) {
         double result = 0;
 
-        parseInfixToPostfix(equation);
-        evaluatePostfix();
+        DSAQueue postfix = parseInfixToPostfix(equation);
+        result = evaluatePostfix(postfix);
 
         return result;
     }
 
-    private DSAQueue parseInfixToPostfix(String equation) {
+    private static DSAQueue parseInfixToPostfix(String equation) {
         DSAStack stackOp = new DSAStack();
         CircularQueue cirQPostfix = new CircularQueue();
+        int index = 0;
 
-        while()
+        while(equation.length() > 0) {
+
+            String[] term = parseTerm(equation);
+            if (term[index].equals("(")) {
+                stackOp.push(term[index]);
+            } else if (term[index].equals(")")) {
+                while(!stackOp.top().equals("(")) {
+                    cirQPostfix.enqueue(stackOp.top());
+                }
+                stackOp.pop();
+            } else if (term[index].equals('+') || term[index].equals('-') || term[index].equals('*') || term[index].equals('/')) {
+                while(!stackOp.isEmpty() && !stackOp.top().equals('(') && (presedenceOf(stackOp.top().toString()) >= presedenceOf(term))) {
+
+                }
+            }
+        }
+
+        return cirQPostfix;
+        
+    }
+ 
+    // https://stackoverflow.com/questions/27808112/java-splitting-with-math-expression
+    private static String[] parseTerm(String equation) {
+        equation =  equation.replaceAll("\\s", "");
+        String[] split = equation.split("(?<=[\\d.])(?=[^\\d.])|(?<=[^\\d.])(?=[\\d.])");
+        return split;
     }
 
-    private double evaluatePostfix(DSAQueue postfixQueue) {
+    private static double evaluatePostfix(DSAQueue postfixQueue) {
         double result = 0;
         return result;
     }
 
-    private int presedenceOf(char theOp) {
-        int result = 0;
-        return result;
+    // https://www.geeksforgeeks.org/java/java-program-to-convert-infix-expression-to-postfix-expression/
+    private static int presedenceOf(char theOp) {
+        switch(theOp) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return 0;
+        }
     }
 
-    private double executeOperation(char op, double op1, double op2) {
+    private static double executeOperation(char op, double op1, double op2) {
         double result = 0;
         return result;
     }
